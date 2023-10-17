d0aceb1c7e95dfe0ef9e651e174e340680fbd908
tshin0 <146213491+tshin0@users.noreply.github.com>
2023-10-17 19:58:20 +0000

    navbar added

--- dice.rb
+++ dice.rb
@@ -1,25 +1,47 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get ("/") do
-  "Hello World"
-end
-
-get ("/zebra") do
-  "We must add a route for each path we want to support"
-end
-
-get ("/giraffe") do
-  "Hopefully this shows up without having to restart the server"
+  erb(:elephant)  
 end
 
 get ("/dice/2/6") do
-  first_die = rand(5)
-  second_die = rand(5)
+  first_die = rand(1..6)
+  second_die = rand(1..6)
   sum = first_die + second_die
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
   
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six, {:layout => :wrapper})
 end
-  
+
+get ("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_ten, {:layout => :wrapper})
+end 
+
+get ("/dice/1/20") do
+  first_die = rand(1..20)
+  @outcome = "You rolled a #{first_die} for a total of #{first_die}."
+
+  erb(:one_twenty, {:layout => :wrapper})
+end
+
+get ("/dice/5/4") do
+  first_die = rand(1..4)
+  second_die = rand(1..4)
+  sum = first_die + second_die
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:five_four, {:layout => :wrapper})
+end 
